59abb340aacf1f4816d27c5d8bdc0434f9182d25
Layan <layan.alahmadi@Outlook.com>
2024-05-13 11:02:49 +0300

    Convert Shipment Class to interface

--- SW_Pattren_Project/src/main/java/supplyChainSystem/Shipment.java
+++ SW_Pattren_Project/src/main/java/supplyChainSystem/Shipment.java
@@ -2,50 +2,16 @@ package supplyChainSystem;
 
 import java.util.Date;
 
-public class Shipment {
-    private int shipmentID;
-    private Order order;
-    private String destination;
-    private Date shipmentDate;
-    private Date arrivalDate;
+//Shipment interface
+public interface Shipment {
 
-    public Shipment(int shipmentID, Order order, String destination, Date shipmentDate, Date arrivalDate) {
-        this.shipmentID = shipmentID;
-        this.order = order;
-        this.destination = destination;
-        this.shipmentDate = shipmentDate;
-        this.arrivalDate = arrivalDate;
-    }
-
-    public int getShipmentID() {
-        return shipmentID;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public Date getShipmentDate() {
-        return shipmentDate;
-    }
-
-    public Date getArrivalDate() {
-        return arrivalDate;
-    }
-
-    public void updateDestination(String newDestination) {
-        this.destination = newDestination;
-    }
-
-    public void updateShipmentDate(Date newShipmentDate) {
-        this.shipmentDate = newShipmentDate;
-    }
-
-    public void updateArrivalDate(Date newArrivalDate) {
-        this.arrivalDate = newArrivalDate;
-    }
+    public int getShipmentID();
+    public Order getOrder();
+    public String getDestination();
+    public Date getShipmentDate();
+    public Date getArrivalDate();
+    public void updateDestination(String newDestination);
+    public void updateShipmentDate(Date newShipmentDate);
+    public void updateArrivalDate(Date newArrivalDate);
+    
 }
